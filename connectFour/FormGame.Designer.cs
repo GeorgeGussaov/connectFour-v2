3f75b1fc02ffb0737a716d16b98adb2c0afd099e
Георгий <gussaov04@gmail.com>
2022-12-31 19:01:37 +0300

    Добавьте файлы проекта.

---
+++ connectFour/FormGame.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace connectFour
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewField = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewField
+            // 
+            this.dataGridViewField.AllowUserToAddRows = false;
+            this.dataGridViewField.AllowUserToDeleteRows = false;
+            this.dataGridViewField.AllowUserToResizeColumns = false;
+            this.dataGridViewField.AllowUserToResizeRows = false;
+            this.dataGridViewField.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewField.ColumnHeadersVisible = false;
+            this.dataGridViewField.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewField.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewField.Location = new System.Drawing.Point(13, 12);
+            this.dataGridViewField.Name = "dataGridViewField";
+            this.dataGridViewField.ReadOnly = true;
+            this.dataGridViewField.RowHeadersVisible = false;
+            this.dataGridViewField.RowTemplate.Height = 100;
+            this.dataGridViewField.Size = new System.Drawing.Size(710, 710);
+            this.dataGridViewField.TabIndex = 0;
+            this.dataGridViewField.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewField_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 732);
+            this.Controls.Add(this.dataGridViewField);
+            this.Name = "FormGame";
+            this.Text = "Четыре в ряд";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewField;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewImageColumn Column2;
+        private System.Windows.Forms.DataGridViewImageColumn Column3;
+        private System.Windows.Forms.DataGridViewImageColumn Column4;
+        private System.Windows.Forms.DataGridViewImageColumn Column5;
+        private System.Windows.Forms.DataGridViewImageColumn Column6;
+        private System.Windows.Forms.DataGridViewImageColumn Column7;
+    }
+}
+
---
+++ connectFour/FormGame.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connectFour
+{
+    public partial class FormGame : Form
+    {
+        Image imgRedCircle, imgOrangeCircle;
+        int SIZE = 7;
+        int[,] field = new int[7, 7];           //поле 7х7
+        int pl = 2;                             //игрок
+        string[] member = {"Игрок1","Игрок2" };
+
+       
+
+
+        public FormGame()
+        {
+            InitializeComponent();
+            dataGridViewField.Rows.Add(SIZE);
+            imgRedCircle = Bitmap.FromFile("images/Red.png");       //ввод картинок кругов доработаю во второй версии, чесна
+            imgOrangeCircle = Bitmap.FromFile("images/orange.png");
+        }
+
+
+        void showField(int[,] mas)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    if (mas[i, j] == 1)
+                    {
+                        dataGridViewField.Rows[i].Cells[j].Value = imgOrangeCircle;
+                        //dataGridViewField.Rows[i].Cells[j].Style.BackColor = Color.Orange;
+                    }
+                    if (mas[i, j] == 2)
+                    {
+                        dataGridViewField.Rows[i].Cells[j].Value = imgRedCircle;
+                        //dataGridViewField.Rows[i].Cells[j].Style.BackColor = Color.OrangeRed;
+                    }
+                }
+
+            }
+
+        }
+
+
+        bool rightStep(int[,] mas)
+        {
+
+            for (int i = SIZE - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    if (mas[i + 1, j] == 0 && mas[i, j] == pl)
+                    {
+                        mas[i, j] = 0;
+                        MessageBox.Show("Выберите нижнюю свободную ячейку!"); //в случае если игрок неверно ходит, автоматическое падение шаров тоже во 2 версии(если смогу)
+                        pl = pl % 2 + 1; //это чтоб после невeрного хода не менялся игрок
+                    }
+                }
+            }
+            return false;
+
+        }
+
+
+
+
+        bool isGameOver(int[,] mas)         //проверка окончания игры
+        {
+
+            for (int i = 0; i < SIZE; i++)   //проверка по строкам и столбцам
+            {
+                int cntStr = 0;
+                int cntStlb = 0;
+
+                for (int j = 0; j < SIZE; j++)
+                {
+                    if (j < 5 && j > 1)
+                        if (mas[i, j] == pl && mas[i, j + 1] == pl && mas[i, j - 1] == pl && (mas[i, j + 2] == pl || mas[i, j - 2] == pl)) cntStr++;
+                    if (i < 5 && i > 1)
+                        if (mas[i, j] == pl && mas[i + 1, j] == pl && mas[i - 1, j] == pl && (mas[i + 2, j] == pl || mas[i - 2, j] == pl)) cntStlb++;
+                }
+
+                if (cntStlb > 0 || cntStr > 0) return true;     
+            }
+
+
+            int mainD = 0;     //проверка по диагоналям
+            int secD = 0;
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    if (j < 5 && j > 1 && i < 5 && i > 1)
+                    {
+                        if (mas[i, j] == pl && mas[i + 1, j + 1] == pl && mas[i - 1, j - 1] == pl && (mas[i + 2, j + 2] == pl || mas[i - 2, j - 2] == pl)) mainD++;
+                        if (mas[i, j] == pl && mas[i - 1, j + 1] == pl && mas[i + 1, j - 1] == pl && (mas[i - 2, j + 2] == pl || mas[i + 2, j - 2] == pl)) secD++;
+                    }
+                }
+            }
+            if (mainD > 0 || secD > 0) return true;
+            return false;
+        }
+
+
+
+
+        private void dataGridViewField_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (field[e.RowIndex, e.ColumnIndex] == 0)
+            {
+                pl = pl % 2 + 1;   //смена хода игрока
+                field[e.RowIndex, e.ColumnIndex] = pl;
+                rightStep(field);
+
+                showField(field);
+                if (isGameOver(field) == true)
+                {
+                    MessageBox.Show("Победил " + member[pl - 1]);
+                    this.Close();
+                }
+            }
+            //MessageBox.Show(e.RowIndex + " " + e.ColumnIndex);
+        }
+
+
+    }
+}
